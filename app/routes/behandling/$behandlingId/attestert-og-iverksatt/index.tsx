b19487ea1981944e9473ea77c67b2f62e7d12871
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-10-22 17:24:54 +0200

    Vis loading state for iverksetter

--- app/routes/behandling/$behandlingId/attestert-og-iverksatt/index.tsx
+++ app/routes/behandling/$behandlingId/attestert-og-iverksatt/index.tsx
@@ -1,29 +1,57 @@
 import { CheckmarkCircleIcon } from '@navikt/aksel-icons'
-import { Button, Heading, HStack, Page, VStack } from '@navikt/ds-react'
-import { redirect } from 'react-router'
+import { Button, Heading, HStack, Loader, Page, VStack } from '@navikt/ds-react'
+import { useEffect } from 'react'
+import { redirect, useRevalidator } from 'react-router'
 import { createBehandlingApi } from '~/api/behandling-api'
 import commonStyles from '~/common.module.css'
 import { AldeBehandlingStatus } from '~/types/behandling'
 import { env } from '~/utils/env.server'
 import type { Route } from './+types'
 
-type Loader = (data: Route.LoaderArgs) => Promise<{ behandlingId: string; psakOppgaveoversikt: string } | Response>
-
-export const loader: Loader = async ({ request, params }) => {
+export const loader = async ({ request, params }: Route.LoaderArgs) => {
   const { behandlingId } = params
   const { psakOppgaveoversikt } = env
 
   const behandling = await createBehandlingApi({ request, behandlingId }).hentBehandling()
 
-  if (behandling.aldeBehandlingStatus === AldeBehandlingStatus.VENTER_ATTESTERING || AldeBehandlingStatus.FULLFORT) {
-    return { behandlingId, psakOppgaveoversikt }
+  if (
+    behandling.aldeBehandlingStatus === AldeBehandlingStatus.VENTER_ATTESTERING ||
+    behandling.aldeBehandlingStatus === AldeBehandlingStatus.FULLFORT
+  ) {
+    return { behandlingId, psakOppgaveoversikt, status: behandling.aldeBehandlingStatus }
   } else {
     return redirect(`/behandling/${behandlingId}`)
   }
 }
 
 const AttestertOgIverksatt = ({ loaderData }: Route.ComponentProps) => {
-  const { psakOppgaveoversikt } = loaderData
+  const { psakOppgaveoversikt, status } = loaderData
+  const revalidator = useRevalidator()
+
+  useEffect(() => {
+    if (status === AldeBehandlingStatus.VENTER_ATTESTERING) {
+      const intervalId = setInterval(() => {
+        revalidator.revalidate()
+      }, 1000)
+
+      return () => clearInterval(intervalId)
+    }
+  }, [status, revalidator])
+
+  if (status === AldeBehandlingStatus.VENTER_ATTESTERING) {
+    return (
+      <Page.Block gutters className={commonStyles.page}>
+        <VStack gap="space-32" className="content" align="center">
+          <Heading size="medium" level="1">
+            Iverksetter
+          </Heading>
+
+          <Loader size="3xlarge" title="Iverksetter" />
+        </VStack>
+      </Page.Block>
+    )
+  }
+
   return (
     <Page.Block gutters className={commonStyles.page}>
       <VStack gap="space-32" className="content" align="center">
