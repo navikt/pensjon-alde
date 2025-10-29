6e1facb5978d78861338d6d58db781312ddc0c23
Per Christian Moen <per.christian.moen@nav.no>
2025-10-29 13:25:13 +0100

    Oppdaterer basert på skisser

--- app/components/FeilendeBehandling.tsx
+++ app/components/FeilendeBehandling.tsx
@@ -1,16 +1,4 @@
-import {
-  BodyLong,
-  BodyShort,
-  Box,
-  Button,
-  CopyButton,
-  Heading,
-  HStack,
-  Link,
-  Page,
-  Spacer,
-  VStack,
-} from '@navikt/ds-react'
+import { BodyLong, BodyShort, Box, Button, CopyButton, Heading, HStack, Link, Page, VStack } from '@navikt/ds-react'
 import commonStyles from '~/common.module.css'
 import type { BehandlingDTO } from '~/types/behandling'
 import { formatDateToNorwegian } from '~/utils/date'
@@ -21,7 +9,7 @@ export default function FeilendeBehandling({ dato, behandling }: { dato: number;
   return (
     <Page>
       <Page.Block gutters className={commonStyles.page} width="md">
-        <VStack gap="8">
+        <VStack gap="4">
           <Heading
             size="xlarge"
             level="1"
@@ -40,52 +28,58 @@ export default function FeilendeBehandling({ dato, behandling }: { dato: number;
               })}.`}
           </BodyLong>
 
-          <BodyLong size="medium">
-            Om ønskelig kan du få Pesys til å forsøke på nytt med en gang eller du kan ta saken til manuell behandling
-          </BodyLong>
-
           <HStack gap="4">
-            <Link href="https://teams.microsoft.com/v2/" target="_blank" rel="noopener noreferrer">
-              Meld fra i Teams
-            </Link>
-            <Button variant={'secondary'}>Ta saken til manuell behandling</Button>
-            <Button variant={'secondary'}>Forsøk på nytt</Button>
+            <Button size="small" variant={'primary'}>
+              Prøv igjen nå
+            </Button>
+            <Button size="small" variant={'secondary'}>
+              Ta saken til manuell behandling
+            </Button>
           </HStack>
 
-          <VStack gap="4">
-            <VStack gap="4">
-              <BodyLong size="medium">
-                <HStack align="center" justify="space-between">
-                  <strong>Feilmelding</strong>
-                  <CopyButton
-                    copyText={
-                      (behandling.sisteKjoring?.feilmelding || '') + ' ' + (behandling.sisteKjoring?.uuid || '')
-                    }
-                    size="small"
-                    variant="action"
-                    text="Kopier"
-                    activeText="Kopiert"
-                  />
-                </HStack>
-              </BodyLong>
+          <BodyLong size="medium">
+            Fortsatt problemer? Vennligst kopier feilmeldingen nedenfor og{' '}
+            <Link href="https://teams.microsoft.com/v2/" target="_blank" rel="noopener noreferrer">
+              meld fra i Teams
+            </Link>
+          </BodyLong>
 
-              <Box.New borderRadius="medium" borderColor="neutral-subtle" borderWidth="1" padding="2">
-                <HStack justify="space-between">
-                  <BodyLong size="small" style={{ padding: '1rem' }}>
-                    {behandling.sisteKjoring?.feilmelding}
-                  </BodyLong>
-                  {behandling.sisteKjoring?.uuid && (
-                    <BodyShort size="small" style={{ padding: '1rem' }} textColor="subtle">
-                      {behandling.sisteKjoring?.uuid}
-                    </BodyShort>
-                  )}
-                </HStack>
-              </Box.New>
+          <VStack gap="4">
+            <VStack>
+              <VStack gap="4">
+                <BodyLong size="medium">
+                  <HStack align="center" justify="space-between">
+                    <strong>Feilmelding</strong>
+                    <CopyButton
+                      copyText={
+                        (behandling.sisteKjoring?.feilmelding || '') + ' ' + (behandling.sisteKjoring?.uuid || '')
+                      }
+                      size="small"
+                      variant="action"
+                      text="Kopier"
+                      activeText="Kopiert"
+                    />
+                  </HStack>
+                </BodyLong>
+
+                <Box.New borderRadius="medium" borderColor="neutral-subtle" borderWidth="1" padding="2">
+                  <HStack justify="space-between">
+                    <BodyLong size="small" style={{ padding: '1rem' }}>
+                      {behandling.sisteKjoring?.feilmelding}
+                    </BodyLong>
+                    {behandling.sisteKjoring?.uuid && (
+                      <BodyShort size="small" style={{ padding: '1rem' }} textColor="subtle">
+                        {behandling.sisteKjoring?.uuid}
+                      </BodyShort>
+                    )}
+                  </HStack>
+                </Box.New>
+              </VStack>
+              <BodyLong size="small" textColor="subtle">
+                {formatDateToNorwegian(dato, { showTime: true })}
+              </BodyLong>
             </VStack>
           </VStack>
-          <BodyLong size="small" textColor="subtle">
-            {formatDateToNorwegian(dato, { showTime: true })}
-          </BodyLong>
         </VStack>
       </Page.Block>
     </Page>
