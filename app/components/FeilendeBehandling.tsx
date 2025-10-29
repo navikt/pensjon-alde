525aa6df8483da09bca0af7242290d64806ec6c6
Per Christian Moen <per.christian.moen@nav.no>
2025-10-29 10:19:09 +0100

    Skiller ut traceid i egen boks

--- app/components/FeilendeBehandling.tsx
+++ app/components/FeilendeBehandling.tsx
@@ -1,4 +1,4 @@
-import { BodyLong, Box, Button, CopyButton, Heading, HStack, Link, Page, VStack } from '@navikt/ds-react'
+import { BodyLong, Box, Button, CopyButton, Heading, HStack, Link, Page, Spacer, VStack } from '@navikt/ds-react'
 import commonStyles from '~/common.module.css'
 import type { BehandlingDTO } from '~/types/behandling'
 import { formatDateToNorwegian } from '~/utils/date'
@@ -37,30 +37,49 @@ export default function FeilendeBehandling({ dato, behandling }: { dato: number;
             <Button variant={'secondary'}>Forsøk på nytt</Button>
           </HStack>
 
-          <VStack gap="1">
-            <BodyLong size="medium">
-              <strong>Feilmelding</strong>
-            </BodyLong>
+          <VStack gap="4">
+            <VStack gap="4">
+              <BodyLong size="medium">
+                <strong>Feilmelding</strong>
+              </BodyLong>
 
-            <Box.New borderRadius="medium" borderColor="neutral-subtle" borderWidth="1" padding="2">
-              <HStack justify="space-between">
-                <BodyLong size="small" style={{ padding: '1rem' }}>
-                  {behandling.sisteKjoring?.feilmelding}
+              <Box.New borderRadius="medium" borderColor="neutral-subtle" borderWidth="1" padding="2">
+                <HStack justify="space-between">
+                  <BodyLong size="small" style={{ padding: '1rem' }}>
+                    {behandling.sisteKjoring?.feilmelding}
+                  </BodyLong>
+                </HStack>
+              </Box.New>
+            </VStack>
+
+            {behandling.sisteKjoring?.uuid && (
+              <>
+                <VStack gap="4">
+                  <BodyLong size="medium">
+                    <strong>Feilsøkningsinformasjon</strong>
+                  </BodyLong>
+
+                  <HStack>
+                    <Box.New borderRadius="medium" borderColor="neutral-subtle" borderWidth="1" padding="2">
+                      <HStack align="center">
+                        {behandling.sisteKjoring?.uuid}
+                        <CopyButton
+                          copyText={behandling.sisteKjoring?.uuid}
+                          size="small"
+                          variant="action"
+                          text="Kopier"
+                          activeText="Kopiert"
+                        />
+                      </HStack>
+                    </Box.New>
+                    <Spacer />
+                  </HStack>
+                </VStack>
+                <BodyLong size="small" textColor="subtle">
+                  {formatDateToNorwegian(dato, { showTime: true })}
                 </BodyLong>
-                {behandling.sisteKjoring?.uuid && (
-                  <CopyButton
-                    copyText={behandling.sisteKjoring?.uuid}
-                    size="small"
-                    variant="action"
-                    text="Kopier"
-                    activeText="Kopiert"
-                  />
-                )}
-              </HStack>
-            </Box.New>
-            <BodyLong size="small" textColor="subtle">
-              {formatDateToNorwegian(dato, { showTime: true })}
-            </BodyLong>
+              </>
+            )}
           </VStack>
         </VStack>
       </Page.Block>
