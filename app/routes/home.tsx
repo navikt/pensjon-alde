0ffddd47c58f1eb93dba202fadcf11fc79dce853
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-11-03 17:04:19 +0100

    Vis generisk "feilside" på rot

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,17 @@
+import { BodyLong, Heading, Page, VStack } from '@navikt/ds-react'
+import commonStyles from '~/common.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h1>hell0o</h1>
-    </div>
+    <Page.Block gutters className={commonStyles.page}>
+      <VStack gap="space-32" className="content" align="center">
+        <Heading size="medium" level="1">
+          <VStack align="center">
+            <Heading size="medium">Mangler behandling</Heading>
+            <BodyLong>Lukk siden og åpne på nytt fra Pesys</BodyLong>
+          </VStack>
+        </Heading>
+      </VStack>
+    </Page.Block>
   )
 }
