77f3c3d4ea091fe4ea47cb4eaf60c3d7c8c038c4
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-12-04 21:06:42 +0100

    Fjern desimaler fra pengesummer

--- app/utils/currency/currency.ts
+++ app/utils/currency/currency.ts
@@ -1,5 +1,10 @@
 export const formatCurrencyNok = (amount: string | number) => {
   const numAmount = typeof amount === 'string' ? parseFloat(amount) : amount
   const safeAmount = Number.isNaN(numAmount) || numAmount === undefined ? 0 : numAmount
-  return new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(safeAmount)
+  return new Intl.NumberFormat('nb-NO', {
+    style: 'currency',
+    currency: 'NOK',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(safeAmount)
 }
